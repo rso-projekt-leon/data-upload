3a6cf715833735185519eba4d488c8647a1ccbbc
Leon Štefanič Južnič <leon11.sj@gamil.com>
2019-11-24 15:31:37 +0100

    changed info point

--- app/api/info.py
+++ app/api/info.py
@@ -9,13 +9,15 @@ class Info(Resource):
     def get(self):
         return {"clani": ["ls4262"],
                 "opis_projekta": "Projekt implementira platformo za podatkovno analizo.",
-                "mikrostoritve": [""],
+                "mikrostoritve": ["http://35.195.92.163:8082/v1/buckets/"],
                 "github": ["https://github.com/rso-projekt-leon/data-upload", 
                            "https://github.com/rso-projekt-leon/data-storage-service",
                            "https://github.com/rso-projekt-leon/data-catalog",
                            "https://github.com/rso-projekt-leon/documentation"],
-                "travis": ["https://travis-ci.org/rso-projekt-leon/data-upload"],
-                "dockerhub": [""]
+                "travis": ["https://travis-ci.org/rso-projekt-leon/data-upload",
+                           "https://travis-ci.org/rso-projekt-leon/data-storage-service"],
+                "dockerhub": ["https://hub.docker.com/repository/docker/leon11sj/data-storage-microservice",
+                              "https://hub.docker.com/repository/docker/leon11sj/data-upload-microservice"]
                 }, 200
 
 
